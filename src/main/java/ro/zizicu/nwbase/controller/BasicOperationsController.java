a055087242a38f31f8556285eca9fbf5701c3eab
Matei Florescu <matei@yahoo.com>
2022-10-09 19:02:39 +0300

    rebuild the hierarchy of services and controllers

--- src/main/java/ro/zizicu/nwbase/controller/BasicOperationsController.java
+++ src/main/java/ro/zizicu/nwbase/controller/BasicOperationsController.java
@@ -2,9 +2,9 @@ package ro.zizicu.nwbase.controller;
 
 import java.io.Serializable;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,22 +20,20 @@ import ro.zizicu.nwbase.exceptions.EntityNotFoundException;
 import ro.zizicu.nwbase.service.CrudService;
 
 
+@Slf4j
+@Data
 public class BasicOperationsController<Entity extends IdentityOwner<ID>, 
 										ID extends Serializable> {
-	
-	private static Logger logger = LoggerFactory.getLogger(BasicOperationsController.class);
-	
-	@Autowired
-	protected CrudService<Entity, ID> service;
+	private final CrudService<Entity, ID> service;
 
-	public void setCrudService(CrudService<Entity, ID> service) {
+	public BasicOperationsController(CrudService<Entity, ID> service) {
 		this.service = service;
 	}
-	
+
 	@GetMapping(value = "/")
 	public ResponseEntity<?> loadAll()
 	{
-		logger.debug("load all entities");
+		log.debug("load all entities");
 		Iterable<Entity> entities = service.loadAll();
 		return ResponseEntity.ok(entities);
 	}
@@ -43,12 +41,12 @@ public class BasicOperationsController<Entity extends IdentityOwner<ID>,
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<?> load(@PathVariable ID id) {
 		try {
-			logger.debug("load entity " + id);
+			log.debug("load entity " + id);
 			return ResponseEntity.ok(service.load(id));
 		}
 		catch(EntityNotFoundException e) {
 			String errorMessage = "entity not found, id:" + id;
-			logger.error(errorMessage);
+			log.error(errorMessage);
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
 		}
 	}
@@ -56,13 +54,13 @@ public class BasicOperationsController<Entity extends IdentityOwner<ID>,
 	@DeleteMapping(value="/")
 	public ResponseEntity<?> delete(@RequestBody Entity entity) {
 		try {
-			logger.debug("delete entity " + entity.getId());
+			log.debug("delete entity " + entity.getId());
 			service.delete(entity);
 			return ResponseEntity.ok("deleted");
 		}
 		catch (Exception e) {
 			String errorMessage = e.getMessage();
-			logger.error(errorMessage, e);
+			log.error(errorMessage, e);
 			return ResponseEntity.badRequest().body(errorMessage);
 		}
 	}
@@ -70,7 +68,7 @@ public class BasicOperationsController<Entity extends IdentityOwner<ID>,
 	@PostMapping(value = "/")
 	public ResponseEntity<?> create(@RequestBody Entity entity) {
 		try {
-			logger.debug("create entity " + entity);
+			log.debug("create entity " + entity);
 			Entity e = service.create(entity);
 			HttpHeaders responseHeaders = new HttpHeaders();
 			responseHeaders.add("Location", getLocation() + e.getId());
@@ -78,21 +76,21 @@ public class BasicOperationsController<Entity extends IdentityOwner<ID>,
 		}
 		catch (Exception e) {
 			String errorMessage = e.getMessage();
-			logger.error(errorMessage, e);
+			log.error(errorMessage, e);
 			return ResponseEntity.badRequest().body(errorMessage);
 		}
 	}
 	
 	@PatchMapping(value = "/{id}") 
 	public ResponseEntity<?> update(@PathVariable ID id, @RequestBody Entity entity) {
-		logger.debug("update category id: " + id);
+		log.debug("update {} with id {}", entity.getEntityName(), id);
 		entity.setId(id);
 		try {
 			return ResponseEntity.ok(service.update(entity));
 		}
 		catch (Exception e) {
 			String errorMessage = e.getMessage();
-			logger.error(errorMessage, e);
+			log.error(errorMessage, e);
 			return ResponseEntity.badRequest().body(errorMessage);
 		}
 	}
--- src/main/java/ro/zizicu/nwbase/controller/NamedEntityController.java
+++ src/main/java/ro/zizicu/nwbase/controller/NamedEntityController.java
@@ -2,9 +2,9 @@ package ro.zizicu.nwbase.controller;
 
 import java.io.Serializable;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,24 +12,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import ro.zizicu.nwbase.entity.NamedIdentityOwner;
 import ro.zizicu.nwbase.service.NamedService;
 
-public class NamedEntityController<Entity extends NamedIdentityOwner<ID>, 
+@Slf4j
+public class NamedEntityController<Entity extends NamedIdentityOwner<ID>,
 									ID extends Serializable> 
 			  extends BasicOperationsController<Entity, ID> {
 
-	private static Logger logger = LoggerFactory.getLogger(NamedEntityController.class);
-	
-	@Autowired
-	private NamedService<Entity, ID> namedService;
-	
+
+	private final NamedService<Entity, ID> namedService;
+
+	public NamedEntityController(NamedService<Entity, ID> namedService) {
+		super(namedService);
+		this.namedService = namedService;
+	}
+
 	@GetMapping(value = "name/{name}")
 	public ResponseEntity<?> findByName(@PathVariable String name) {
-		logger.debug("find: " + name);
+		log.debug("find: " + name);
 		try {
 			return ResponseEntity.ok(namedService.loadByName(name));
 		}
 		catch (Exception e) {
 			String errorMessage = e.getMessage();
-			logger.error(errorMessage, e);
+			log.error(errorMessage, e);
 			return ResponseEntity.badRequest().body(errorMessage);
 		}
 		
