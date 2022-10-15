78540ba0a55c6f65b293040e14872985ef50c2dc
Matei Florescu <matei@yahoo.com>
2022-10-15 15:24:02 +0300

    rebuild the hierarchy of services and controllers

--- src/main/java/ro/zizicu/nwbase/service/impl/CrudServiceImpl.java
+++ src/main/java/ro/zizicu/nwbase/service/impl/CrudServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,32 +15,31 @@ import ro.zizicu.nwbase.entity.IdentityOwner;
 import ro.zizicu.nwbase.exceptions.EntityNotFoundException;
 import ro.zizicu.nwbase.service.CrudService;
 
-
+@Slf4j
 public class CrudServiceImpl<Entity extends IdentityOwner<ID>, 
 							  ID extends Serializable> 
 	implements CrudService<Entity, ID>
 
 {
-	private static Logger logger = LoggerFactory.getLogger(CrudServiceImpl.class);
-	
+
 	@Autowired
 	protected CrudRepository<Entity, ID> repository;
 
 	@Override
 	public void delete(Entity entity) {
-		if(logger.isInfoEnabled()) logger.info("delete: " + entity.getClass().getName() + " id " + entity.getId());
+		if(log.isInfoEnabled()) log.info("delete: " + entity.getClass().getName() + " id " + entity.getId());
 		repository.delete(entity);
 	}
 	
 	@Override
 	public Entity create(Entity entity) {
-		if(logger.isInfoEnabled()) logger.info("create: " + entity.getClass().getName() + " id " + entity.getId());
+		if(log.isInfoEnabled()) log.info("create: " + entity.getClass().getName() + " id " + entity.getId());
 		return repository.save(entity);
 	}
 	
 	@Override
 	public List<Entity> loadAll() {
-		if(logger.isInfoEnabled()) logger.info("load all entities");
+		if(log.isInfoEnabled()) log.info("load all entities");
 		List<Entity> entities = new ArrayList<>();
 		
 		for(Entity e : repository.findAll())
@@ -50,10 +50,10 @@ public class CrudServiceImpl<Entity extends IdentityOwner<ID>,
 	
 	@Override
 	public Entity load(ID id) {
-		if(logger.isInfoEnabled()) logger.info("load entity with id " + id);
+		if(log.isInfoEnabled()) log.info("load entity with id " + id);
 		Optional<Entity> entity = repository.findById(id);
 		if(entity.isEmpty()) {
-			logger.info("entity with id {} not found", id);
+			log.info("entity with id {} not found", id);
 			throw new EntityNotFoundException();
 		}
 		return entity.get();
@@ -62,7 +62,7 @@ public class CrudServiceImpl<Entity extends IdentityOwner<ID>,
 
 	@Override
 	public Entity update(Entity entity) {
-		if(logger.isInfoEnabled()) logger.info("update: " + entity.getClass().getName() + " id " + entity.getId());
+		if(log.isInfoEnabled()) log.info("update: " + entity.getClass().getName() + " id " + entity.getId());
 		return repository.save(entity);
 	}
 }
