febd914f216b8fe599f55486b18837aae855bc0d
Matei <mateifl@yahoo.com>
2022-11-05 18:10:42 +0200

    More work on 2 phase commit

--- src/main/java/ro/zizicu/nwbase/transaction/ExecuteTransactionMessage.java
+++ src/main/java/ro/zizicu/nwbase/transaction/ExecuteTransactionMessage.java
@@ -6,11 +6,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Payload for REST POST that will commit/rollback a transaction.
+ * transactionResult = "COMMIT" or "ROLLBACK"
+ */
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CommitMessage {
+public class ExecuteTransactionMessage {
     private String microservice;
     private Long transactionId;
+    private String transactionResult;
 }
